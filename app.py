547fe86d90d3b4b763265e6b2d84d5e2f19d80f8
Aitor <aitirga@gmail.com>
2021-08-04 19:44:03 +0200

    feat: add basic web files

--- app.py
+++ app.py
@@ -7,5 +7,10 @@ app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
 
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
 if __name__ == '__main__':
-    app.run (debug=True, use_reloader=True, host="0.0.0.0")
+    app.run(debug=True, use_reloader=True)
